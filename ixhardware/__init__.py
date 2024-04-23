38dfd13f48a0b2aa0384e2e3f46931d150ae4d2c
M. Rehan <mrehanlm93@gmail.com>
2024-04-23 19:54:41 +0500

    Add changes to retrieve bhyve product name

--- ixhardware/__init__.py
+++ ixhardware/__init__.py
@@ -1,4 +1,10 @@
-from .chassis import PLATFORM_PREFIXES, TRUENAS_UNKNOWN, get_chassis_hardware
+from .chassis import (
+    PLATFORM_PREFIXES, TRUENAS_UNKNOWN, get_chassis_hardware, get_bhyve_hardware_and_node,
+    TRUENAS_QEMU, TRUENAS_BHYVE,
+)
 from .dmi import DMIInfo, DMIParser, parse_dmi
 
-__all__ = ["PLATFORM_PREFIXES", "TRUENAS_UNKNOWN", "get_chassis_hardware", "DMIInfo", "DMIParser", "parse_dmi"]
+__all__ = [
+    "PLATFORM_PREFIXES", "TRUENAS_UNKNOWN", "get_chassis_hardware", "DMIInfo", "DMIParser", "parse_dmi",
+    "get_bhyve_hardware_and_node", "TRUENAS_QEMU", "TRUENAS_BHYVE",
+]
--- ixhardware/chassis.py
+++ ixhardware/chassis.py
@@ -1,6 +1,13 @@
+import typing
+
+from pyudev import Context
+
 from .dmi import DMIInfo
 
-__all__ = ["PLATFORM_PREFIXES", "TRUENAS_UNKNOWN", "get_chassis_hardware"]
+__all__ = [
+    "PLATFORM_PREFIXES", "TRUENAS_UNKNOWN", "get_chassis_hardware", "TRUENAS_BHYVE", "TRUENAS_QEMU",
+    "get_bhyve_hardware_and_node",
+]
 
 
 # We tag SMBIOS with relevant strings for each platform
@@ -16,6 +23,7 @@ PLATFORM_PREFIXES = (
     "TRUENAS-R",  # freenas certified replacement
     "FREENAS-MINI",  # minis tagged with legacy information
 )
+TRUENAS_BHYVE = "BHYVE"
 TRUENAS_QEMU = "IXKVM"
 TRUENAS_UNKNOWN = "TRUENAS-UNKNOWN"
 
@@ -30,9 +38,33 @@ def get_chassis_hardware(dmi: DMIInfo):
         # last resort
         return "TRUENAS-X"
 
-    if dmi.system_product_name == "qemu" and dmi.system_serial_number.startswith(
+    elif dmi.system_product_name == "qemu" and dmi.system_serial_number.startswith(
         "ha"
     ) and dmi.system_serial_number.endswith(("_c1", "_c2")):
         return TRUENAS_QEMU
 
+    elif dmi.system_product_name == "BHYVE":
+        if get_bhyve_hardware_and_node()[0] == "BHYVE":
+            return TRUENAS_BHYVE
+
     return TRUENAS_UNKNOWN
+
+
+def get_bhyve_hardware_and_node() -> typing.Tuple[str, str]:
+    # bhyve host configures a scsi_generic device that when sent an inquiry will
+    # respond with a string that we use to determine the position of the node
+    HARDWARE = NODE = "MANUAL"
+    ctx = Context()
+    for i in ctx.list_devices(subsystem="scsi_generic"):
+        if (model := i.attributes.get("device/model")) is not None:
+            model = model.decode().strip() if isinstance(model, bytes) else model.strip()
+            if model == "TrueNAS_A":
+                NODE = "A"
+                HARDWARE = "BHYVE"
+                break
+            elif model == "TrueNAS_B":
+                NODE = "B"
+                HARDWARE = "BHYVE"
+                break
+
+    return HARDWARE, NODE
